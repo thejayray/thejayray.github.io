eea7f79c9178c57fb1d6cf345b4290bd19bd910b
Jay Ray <jeremiah.ray1@gmail.com>
2019-03-21 23:14:07 -0500

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9613,
-	"created_at": "Sat Mar 09 04:14:06 +0000 2019",
+	"tweets": 9614,
+	"created_at": "Fri Mar 22 04:14:06 +0000 2019",
 	"lang": "en"
 }
--- twitter/data/js/tweets/2019_03.js
+++ twitter/data/js/tweets/2019_03.js
@@ -1,6 +1,145 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Thu Mar 21 15:29:56 +0000 2019",
+		"id": 1108752610318798800,
+		"id_str": "1108752610318798849",
+		"full_text": "Thanks so much to @DeRay for speaking at LibTech 2019. Great talk; great to meet you in person finally!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "deray",
+					"name": "deray",
+					"id": 29417304,
+					"id_str": "29417304",
+					"indices": [
+						18,
+						24
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 10434612,
+			"id_str": "10434612",
+			"name": "Jay Ray",
+			"screen_name": "JayRay",
+			"location": "Madison, WI",
+			"description": "Technologist, Father, Connoisseur of Everything.  I’m trying something new: all tweets are automatically generated from my blog.  All ellipses are my own…",
+			"url": "https://t.co/wHxeFn3mK4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wHxeFn3mK4",
+							"expanded_url": "https://engineeredeloquence.com",
+							"display_url": "engineeredeloquence.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 473,
+			"friends_count": 369,
+			"listed_count": 16,
+			"created_at": "Wed Nov 21 06:39:02 +0000 2007",
+			"favourites_count": 867,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9388,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F2EEE8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/891830913797160960/SN4cgFnZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/891830913797160960/SN4cgFnZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/10434612/1501464070",
+			"profile_link_color": "BD2C2F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "60e2c37980197297",
+			"url": "https://api.twitter.com/1.1/geo/id/60e2c37980197297.json",
+			"place_type": "city",
+			"name": "St Paul",
+			"full_name": "St Paul, MN",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-93.207783,
+							44.8907521
+						],
+						[
+							-93.003514,
+							44.8907521
+						],
+						[
+							-93.003514,
+							44.992279
+						],
+						[
+							-93.207783,
+							44.992279
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Thanks so much to @DeRay for speaking at LibTech 2019. Great talk; great to meet you in person finally!"
+	},
+	{
 		"created_at": "Fri Mar 08 18:03:43 +0000 2019",
 		"id": 1104080268456091600,
 		"id_str": "1104080268456091650",
