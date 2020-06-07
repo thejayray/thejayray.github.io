e48980f470c05f769b9f84018962b91f8b0284bf
Jay Ray <jeremiah.ray1@gmail.com>
2020-06-06 23:14:08 -0500

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9670,
-	"created_at": "Sat Jun 06 04:14:07 +0000 2020",
+	"tweets": 9671,
+	"created_at": "Sun Jun 07 04:14:07 +0000 2020",
 	"lang": "en"
 }
--- twitter/data/js/tweets/2020_06.js
+++ twitter/data/js/tweets/2020_06.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sat Jun 06 11:24:01 +0000 2020",
+		"id": 1269169682059268000,
+		"id_str": "1269228548306415616",
+		"full_text": "The local chapter of Black Lives Matter said it did not support painting the street and called it a distraction from the D.C. mayor's \"active counter organizing to our demands to decrease the police budget and invest in the community.” https://t.co/E3xxeo9rdT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/E3xxeo9rdT",
+					"expanded_url": "https://bit.ly/2MAPzBP",
+					"display_url": "bit.ly/2MAPzBP",
+					"indices": [
+						236,
+						259
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.socialflow.com\" rel=\"nofollow\">SocialFlow</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8861752,
+			"id_str": "8861752",
+			"name": "The Baltimore Sun",
+			"screen_name": "baltimoresun",
+			"location": "Baltimore, Maryland",
+			"description": "Light for all.",
+			"url": "https://t.co/92eEJALtbC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/92eEJALtbC",
+							"expanded_url": "https://www.baltimoresun.com/",
+							"display_url": "baltimoresun.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 333374,
+			"friends_count": 12006,
+			"listed_count": 3976,
+			"created_at": "Thu Sep 13 20:21:39 +0000 2007",
+			"favourites_count": 2609,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 198117,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "75CBF0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/520989247491166209/blL63-Iz_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/520989247491166209/blL63-Iz_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8861752/1550072197",
+			"profile_link_color": "15225E",
+			"profile_sidebar_border_color": "C6E2EE",
+			"profile_sidebar_fill_color": "FFE600",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 210,
+		"favorite_count": 366,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The local chapter of Black Lives Matter said it did not support painting the street and called it a distraction from the D.C. mayor's \"active counter organizing to our demands to decrease the police budget and invest in the community.” https://t.co/E3xxeo9rdT"
+	},
+	{
 		"created_at": "Fri Jun 05 17:29:02 +0000 2020",
 		"id": 1268916115809489000,
 		"id_str": "1268958020861558786",
