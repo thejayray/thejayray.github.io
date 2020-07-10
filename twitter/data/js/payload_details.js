04005a932ab4259db3b4202265f9cd283fd0bb75
Jay Ray <jeremiah.ray1@gmail.com>
2020-07-09 23:14:08 -0500

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9740,
-	"created_at": "Thu Jul 09 04:14:08 +0000 2020",
+	"tweets": 9741,
+	"created_at": "Fri Jul 10 04:14:07 +0000 2020",
 	"lang": "en"
 }
--- twitter/data/js/tweets/2020_07.js
+++ twitter/data/js/tweets/2020_07.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 09 21:00:52 +0000 2020",
+		"id": 1281291986687758300,
+		"id_str": "1281332516503326720",
+		"full_text": "It occurs to me that we could stay in this state for over a year, vaccine delayed, virus/masks so thoroughly politicized that there's no erasing the damage, America sealed off from the rest of the world, economy crushed while the world moves on without us. Have a nice day!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 48428463,
+			"id_str": "48428463",
+			"name": "Heather Havrilesky",
+			"screen_name": "hhavrilesky",
+			"location": "\"The balcony of Southern CA\"",
+			"description": "Ask Polly columnist @TheCut, Ask Molly newsletter-er, author of What If This Were Enough? (https://t.co/VUYfQNn6cl) (illustration by Michael Sowa)",
+			"url": "https://t.co/PfT7CrCc9w",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PfT7CrCc9w",
+							"expanded_url": "https://askmolly.substack.com/",
+							"display_url": "askmolly.substack.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VUYfQNn6cl",
+							"expanded_url": "http://bit.ly/2xQc5iX",
+							"display_url": "bit.ly/2xQc5iX",
+							"indices": [
+								91,
+								114
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 53017,
+			"friends_count": 2969,
+			"listed_count": 783,
+			"created_at": "Thu Jun 18 17:49:08 +0000 2009",
+			"favourites_count": 22268,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 33110,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C6E2EE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1034560252752982016/RTcXnioZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1034560252752982016/RTcXnioZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/48428463/1535493300",
+			"profile_link_color": "696969",
+			"profile_sidebar_border_color": "C6E2EE",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 177,
+		"favorite_count": 1418,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "It occurs to me that we could stay in this state for over a year, vaccine delayed, virus/masks so thoroughly politicized that there's no erasing the damage, America sealed off from the rest of the world, economy crushed while the world moves on without us. Have a nice day!"
+	},
+	{
 		"created_at": "Wed Jul 08 22:42:35 +0000 2020",
 		"id": 1280995726735147000,
 		"id_str": "1280995726735147008",
