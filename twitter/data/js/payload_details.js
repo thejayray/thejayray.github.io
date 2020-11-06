1bf71691b4dde94903a2086e0963fec3957cee10
Jay Ray <jeremiah.ray1@gmail.com>
2020-11-05 22:14:08 -0600

    Added 2 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9766,
-	"created_at": "Thu Nov 05 04:14:07 +0000 2020",
+	"tweets": 9768,
+	"created_at": "Fri Nov 06 04:14:07 +0000 2020",
 	"lang": "en"
 }
--- twitter/data/js/tweets/2020_11.js
+++ twitter/data/js/tweets/2020_11.js
@@ -1,6 +1,286 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 05 22:57:43 +0000 2020",
+		"id": 1324486076451430400,
+		"id_str": "1324486076451430402",
+		"full_text": "@EllaTschopik Future me will be like, “Cry more, snowflake.” \n\nWait… that’s current me.",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			87
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EllaTschopik",
+					"name": "Ella",
+					"id": 22310828,
+					"id_str": "22310828",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1324481189856596000,
+		"in_reply_to_status_id_str": "1324481189856595971",
+		"in_reply_to_user_id": 22310828,
+		"in_reply_to_user_id_str": "22310828",
+		"in_reply_to_screen_name": "EllaTschopik",
+		"user": {
+			"id": 10434612,
+			"id_str": "10434612",
+			"name": "Jay Ray",
+			"screen_name": "JayRay",
+			"location": "Madison, WI",
+			"description": "This account is now used to amplify the voices of BIPOC. They deserve our attention, respect, and support. #BlackLivesMatter. he/him/his",
+			"url": "https://t.co/wHxeFn3mK4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wHxeFn3mK4",
+							"expanded_url": "https://engineeredeloquence.com",
+							"display_url": "engineeredeloquence.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 467,
+			"friends_count": 258,
+			"listed_count": 15,
+			"created_at": "Wed Nov 21 06:39:02 +0000 2007",
+			"favourites_count": 927,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9504,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F2EEE8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1275271951901212673/LP7lEJQc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1275271951901212673/LP7lEJQc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/10434612/1592883622",
+			"profile_link_color": "BD2C2F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@EllaTschopik Future me will be like, “Cry more, snowflake.” \n\nWait… that’s current me."
+	},
+	{
+		"created_at": "Thu Nov 05 21:17:25 +0000 2020",
+		"id": 1323836686883790800,
+		"id_str": "1324460837248131073",
+		"full_text": "The First. https://t.co/h3o0GxeFLR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			10
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1323836500853796900,
+					"id_str": "1323836500853796864",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/media/El82SJYWMAAziA1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/El82SJYWMAAziA1.jpg",
+					"url": "https://t.co/h3o0GxeFLR",
+					"display_url": "pic.twitter.com/h3o0GxeFLR",
+					"expanded_url": "https://twitter.com/CoriBush/status/1323836686883790850/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 567,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 321,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 968,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1323836500853796900,
+					"id_str": "1323836500853796864",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/media/El82SJYWMAAziA1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/El82SJYWMAAziA1.jpg",
+					"url": "https://t.co/h3o0GxeFLR",
+					"display_url": "pic.twitter.com/h3o0GxeFLR",
+					"expanded_url": "https://twitter.com/CoriBush/status/1323836686883790850/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 567,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 321,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 968,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4696552878,
+			"id_str": "4696552878",
+			"name": "Cori Bush",
+			"screen_name": "CoriBush",
+			"location": "St Louis, MO",
+			"description": "Congresswoman-elect for #MO01, RN, Ferguson Activist, Fmr National Bernie 2020 Surrogate • I Am the People I Fight For • #fromProtest2Politics • She/her",
+			"url": "https://t.co/elIAuyBdca",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/elIAuyBdca",
+							"expanded_url": "http://www.coribush.org/vote",
+							"display_url": "coribush.org/vote",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 272925,
+			"friends_count": 2142,
+			"listed_count": 1011,
+			"created_at": "Sat Jan 02 14:36:12 +0000 2016",
+			"favourites_count": 12466,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7050,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1275804043785334784/7j5yj4ma_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1275804043785334784/7j5yj4ma_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4696552878/1604607334",
+			"profile_link_color": "2E529E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 17759,
+		"favorite_count": 147419,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The First. https://t.co/h3o0GxeFLR"
+	},
+	{
 		"created_at": "Wed Nov 04 22:56:42 +0000 2020",
 		"id": 1324123436084506600,
 		"id_str": "1324123436084506626",
