8544ec3b98aa53e79c16465528c159b24c4085c8
Jay Ray <jeremiah.ray1@gmail.com>
2021-01-06 22:14:07 -0600

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9799,
-	"created_at": "Sat Dec 12 04:14:06 +0000 2020",
+	"tweets": 9800,
+	"created_at": "Thu Jan 07 04:14:07 +0000 2021",
 	"lang": "en"
 }
---
+++ twitter/data/js/tweets/2021_01.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2021_01 = 
+[
+	{
+		"created_at": "Wed Jan 06 22:59:35 +0000 2021",
+		"id": 1346928726626467800,
+		"id_str": "1346954597227782144",
+		"full_text": "to my black activists, specifically the ones in DC, do not put your black body on the line to defend a government that has never defended you. \n\nplease stay inside and stay safe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4808077708,
+			"id_str": "4808077708",
+			"name": "Mariah Cooley",
+			"screen_name": "mariahbcooley",
+			"location": "",
+			"description": "Activist & Future Elected Official|BLM| Howard University |previously: @repcheri currently: @transition46| @marchforourlives |she/her | views are my own",
+			"url": "https://t.co/xzj7ar1gze",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xzj7ar1gze",
+							"expanded_url": "http://instagram.com/mariahbcooley",
+							"display_url": "instagram.com/mariahbcooley",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1765,
+			"friends_count": 1368,
+			"listed_count": 5,
+			"created_at": "Thu Jan 14 21:58:13 +0000 2016",
+			"favourites_count": 7157,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2413,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1318712434174156802/GHb9Qxrl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1318712434174156802/GHb9Qxrl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4808077708/1592229178",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 31909,
+		"favorite_count": 128561,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "to my black activists, specifically the ones in DC, do not put your black body on the line to defend a government that has never defended you. \n\nplease stay inside and stay safe"
+	}
+]
