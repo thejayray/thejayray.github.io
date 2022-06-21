627f3bf5b28a18294a3015c9feb2346de14be889
Jay Ray <jeremiah.ray1@gmail.com>
2022-06-20 23:14:07 -0500

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9829,
-	"created_at": "Wed Jun 08 04:14:06 +0000 2022",
+	"tweets": 9830,
+	"created_at": "Tue Jun 21 04:14:06 +0000 2022",
 	"lang": "en"
 }
--- twitter/data/js/tweets/2022_06.js
+++ twitter/data/js/tweets/2022_06.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Mon Jun 20 15:33:51 +0000 2022",
+		"id": 1538907990383812600,
+		"id_str": "1538907990383812615",
+		"full_text": "Two more weeks to apply for an opportunity to work with a group at UW-Madison who is focused on diversity, equity, and inclusion in the tech and library industries. We work hard to welcome and support all who engage with us. https://t.co/lKsnH1Br1B",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			224
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lKsnH1Br1B",
+					"expanded_url": "https://twitter.com/JayRay/status/1534164197919031298",
+					"display_url": "twitter.com/JayRay/status/…",
+					"indices": [
+						225,
+						248
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 10434612,
+			"id_str": "10434612",
+			"name": "Jay Ray",
+			"screen_name": "JayRay",
+			"location": "Madison, WI",
+			"description": "This account is now used to amplify the voices of BIPOC. They deserve our attention, respect, and support. #BlackLivesMatter. he/him/his",
+			"url": "https://t.co/wHxeFn3mK4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wHxeFn3mK4",
+							"expanded_url": "https://engineeredeloquence.com",
+							"display_url": "engineeredeloquence.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 441,
+			"friends_count": 265,
+			"listed_count": 14,
+			"created_at": "Wed Nov 21 06:39:02 +0000 2007",
+			"favourites_count": 907,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9520,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F2EEE8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1275271951901212673/LP7lEJQc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1275271951901212673/LP7lEJQc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/10434612/1592883622",
+			"profile_link_color": "BD2C2F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1534164197919031300,
+		"quoted_status_id_str": "1534164197919031298",
+		"quoted_status_permalink": {
+			"url": "https://t.co/lKsnH1Br1B",
+			"expanded": "https://twitter.com/JayRay/status/1534164197919031298",
+			"display": "twitter.com/JayRay/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jun 07 13:23:42 +0000 2022",
+			"id": 1534164197919031300,
+			"id_str": "1534164197919031298",
+			"full_text": "New job opening in my unit. We do awesome work, supporting library patrons and working with campus partners to provide best-in-class technology to all. Join us in our important work at UW-Madison.\n\nCloses July 5 and we accept student experience.\n\nJob here: https://t.co/KxkMHFQ7gg",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/KxkMHFQ7gg",
+						"expanded_url": "https://jobs.hr.wisc.edu/en-us/job/513929/library-technology-support-specialist",
+						"display_url": "jobs.hr.wisc.edu/en-us/job/5139…",
+						"indices": [
+							257,
+							280
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 10434612,
+				"id_str": "10434612",
+				"name": "Jay Ray",
+				"screen_name": "JayRay",
+				"location": "Madison, WI",
+				"description": "This account is now used to amplify the voices of BIPOC. They deserve our attention, respect, and support. #BlackLivesMatter. he/him/his",
+				"url": "https://t.co/wHxeFn3mK4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/wHxeFn3mK4",
+								"expanded_url": "https://engineeredeloquence.com",
+								"display_url": "engineeredeloquence.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 441,
+				"friends_count": 265,
+				"listed_count": 14,
+				"created_at": "Wed Nov 21 06:39:02 +0000 2007",
+				"favourites_count": 907,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 9520,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F2EEE8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1275271951901212673/LP7lEJQc_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1275271951901212673/LP7lEJQc_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/10434612/1592883622",
+				"profile_link_color": "BD2C2F",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "99CC33",
+				"profile_text_color": "3E4415",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Two more weeks to apply for an opportunity to work with a group at UW-Madison who is focused on diversity, equity, and inclusion in the tech and library industries. We work hard to welcome and support all who engage with us. https://t.co/lKsnH1Br1B"
+	},
+	{
 		"created_at": "Tue Jun 07 19:18:01 +0000 2022",
 		"id": 1534253364950999000,
 		"id_str": "1534253364950999040",
