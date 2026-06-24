360e8b4437eb042218d1c77cc29c1cece7416ac5
Jay Ray <jeremiah.ray1@gmail.com>
2026-06-24 11:58:29 -0500

    Moving social archives to old directory

--- EloquentFeed/data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details =  {
-  "tweets" : 454,
-  "created_at" : "2017-06-02 15:22:34 +0000",
-  "lang" : "en"
-}
--- twitter/data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details = {
-	"tweets": 9840,
-	"created_at": "Mon Oct 17 04:14:07 +0000 2022",
-	"lang": "en"
-}
